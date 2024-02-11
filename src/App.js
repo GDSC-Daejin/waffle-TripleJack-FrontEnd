7adffdf196185f7ca9e368ac4c04f4a0c351d50d
Jun0729 <choiminjun0729@gmail.com>
2024-02-11 23:50:09 +0900

    flex

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Button,Container,Form,Navbar} from 'react-bootstrap';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+
+
+      <Navbar className="bg-body-tertiary">
+      <Container fluid>
+        <Navbar.Brand href="#">Daejin Univ</Navbar.Brand>
+      
+       
+          <Form className="d-flex">
+            <Form.Control
+              type="search"
+              placeholder="지역명으로 검색"
+              className="me-2"
+              aria-label="Search"
+            />
+            <Button variant="outline-success">Search</Button>
+          </Form>
+       
+      </Container>
+    </Navbar>
+
+    <div>
+      
     </div>
+
+
+
+
+
+    </div>
+    
   );
 }
 
