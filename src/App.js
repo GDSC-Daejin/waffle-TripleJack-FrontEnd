c7e7cb9f011d0ae9f4ad4af7b8ac46d813e5e8c7
Jun0729 <choiminjun0729@gmail.com>
2024-02-12 19:05:24 +0900

    idpw

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import {Button,Container,Form,Navbar} from 'react-bootstrap';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Button, Container, Form, Navbar } from "react-bootstrap";
 
 function App() {
   return (
-    <div className='container'>
-
-
+    <div className="container">
       <Navbar className="bg-body-tertiary">
-      <Container fluid>
-        <Navbar.Brand href="#">Daejin Univ</Navbar.Brand>
-      
-       
+        <Container fluid>
+          <Navbar.Brand href="#">Daejin Univ</Navbar.Brand>
+
           <Form className="d-flex">
             <Form.Control
               type="search"
@@ -21,20 +18,16 @@ function App() {
             />
             <Button variant="outline-success">Search</Button>
           </Form>
-       
-      </Container>
-    </Navbar>
+        </Container>
+      </Navbar>
 
-    <div>
-      
+      <div className="logo">로고</div>
+
+      <div className="InputWrap">
+        <input className="idpw" type="text" placeholder="아이디 입력"></input>
+        <input className="idpw" type="text" placeholder="비밀번호 입력"></input>
+      </div>
     </div>
-
-
-
-
-
-    </div>
-    
   );
 }
 
