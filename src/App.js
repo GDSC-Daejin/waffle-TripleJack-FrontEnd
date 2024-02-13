3b3e0270cee38feedefe43588b3b246608e946bf
Jun0729 <choiminjun0729@gmail.com>
2024-02-13 23:39:29 +0900

    firstLogin

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Button, Container, Form, Navbar } from "react-bootstrap";
+import google from "./google.png";
+import React from "react";
 
 function App() {
   return (
-    <div className="container">
+    <div className="contentWrap">
       <Navbar className="bg-body-tertiary">
         <Container fluid>
           <Navbar.Brand href="#">Daejin Univ</Navbar.Brand>
@@ -26,6 +28,34 @@ function App() {
       <div className="InputWrap">
         <input className="idpw" type="text" placeholder="아이디 입력"></input>
         <input className="idpw" type="text" placeholder="비밀번호 입력"></input>
+      </div>
+
+      <div className="loginBtnWrap">
+        <button className="loginBtn">로그인</button>
+      </div>
+
+      <div className="findIdPw">
+        <span
+          className="findId"
+          style={{ marginRight: "30px" }}
+          onClick={() => {
+            alert("");
+          }}
+        >
+          아이디찾기
+        </span>
+        <span>|</span>
+        <span className="findPw" style={{ marginLeft: "30px" }}>
+          비밀번호 찾기
+        </span>
+      </div>
+
+      <div className="google">
+        <img src={google} className="googleLogo" />
+      </div>
+
+      <div className="email">
+        <button className="emailBtn">이메일로 가입하기</button>
       </div>
     </div>
   );
